58e8a4a65d803d16ea3979a4a0c1b02e515158f3
hcampbellsills <hcampbellsills@gmail.com>
2019-09-05 00:18:24 +0200

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix() returns a list of 4 functions :
+## set() pushes the matrix to the cache
+## get() pulls the matrix from the cache
+## setinverse() pushes the matrix solved by the solve() function of cachesolve()
+## getinverse() pulls a previously solved matrix from the cache
+## none of these functions are called during the execution of makeCacheMatrix(); they are only defined
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve() uses the getinverse() function in order to evaluate
+## if a matrix contained in an object returned by makeCacheMatrix()
+## (let's say myCachedMatrix) has already been solved. Two possible scenarios:
+## 1) If the matrix has not been solved yet (i.e. getinverse() returns NULL), it does the calculation
+## and also pushes the solved matrix to the myCachedMatrix via the setinverse() function.
+## 2) If the matrix has already been solved (i.e. getinverse() does not return NULL), it uses the
+## getinverse() function to pull the value stored in the myCachedMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if(is.null(x$getinverse())) {
+    x$setinverse(solve(x$get(), ...))
+    return(x$getinverse())
+  } else {
+    message("getting cached data")
+    return(x$getinverse())
+  }
 }
